128b9b2ff03a37be2d8b7fe65db9d0647c78b0a6
Mooftwo2 <122929359+Mooftwo2@users.noreply.github.com>
2023-02-22 19:37:35 -0700

    implemented camera capture activity

---
+++ app/src/main/java/com/example/pokemonqrcode/CaptureAct.java
@@ -0,0 +1,6 @@
+package com.example.pokemonqrcode;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity {
+}
--- app/src/main/java/com/example/pokemonqrcode/MainActivity.java
+++ app/src/main/java/com/example/pokemonqrcode/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.pokemonqrcode;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton cameraButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cameraButton = findViewById(R.id.open_camera_button);
+        cameraButton.setOnClickListener(v->
+        {
+            scanCode();
+        });
+
     }
+
+    private void scanCode() {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to turn on flash");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLauncher.launch(options);
+    }
+
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result->
+    {
+        if(result.getContents() !=null) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            }).show();
+        }
+    });
 }
