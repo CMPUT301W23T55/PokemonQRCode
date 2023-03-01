07799f0cadaabb74d985d87e351784a343fa8778
Mooftwo2 <122929359+Mooftwo2@users.noreply.github.com>
2023-02-28 18:36:35 -0700

    added PlayerCode class
    this will take info from the ScannedCode and create a new object which is what goes on the player's profile

---
+++ app/src/main/java/com/example/pokemonqrcode/PlayerCode.java
@@ -0,0 +1,18 @@
+package com.example.pokemonqrcode;
+
+/**
+ * This class is what shows up on the player profile
+ * Create custom ListView adapter similar to GasStation in Assignment1 
+ */
+
+public class PlayerCode {
+    /*
+    name
+    score
+    date
+    picture
+    geolocation
+    comment
+
+     */
+}
