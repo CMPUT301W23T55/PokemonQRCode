492f56b22b8bde2b4aabccef91dc63f9428c016f
CarterWP01 <zekewp101@gmail.com>
2023-03-06 22:59:02 -0700

    added database to app, and added a total score method

---
+++ app/src/main/java/com/example/pokemonqrcode/FireStoreClass.java
@@ -0,0 +1,94 @@
+package com.example.pokemonqrcode;
+
+import android.util.Log;
+import android.util.Pair;
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import java.util.HashMap;
+
+public class FireStoreClass {
+
+    public static int totalSum;
+    private final String userName;
+    private FirebaseFirestore db;
+
+    public FireStoreClass(String userName){
+        this.userName = userName;
+    }
+
+    /**
+     * To set up the collection to that particular user, only is created once
+     */
+    void firstTimeLogIn(){
+
+    }
+
+    /**
+     * Adds a scanned QR code to the users collection
+     * @param name name of scanned QR code
+     * @param score score of the scanned QR code
+     * @param location location where QR code was scanned
+     * @param hashcode Scanned hashcode
+     */
+    public void addAQRCode(String name, int score, Pair<Integer, Integer> location,
+                    int hashcode){
+
+        db = FirebaseFirestore.getInstance();
+
+        HashMap<String, Object> data = new HashMap<>();
+
+        data.put("Name",name);
+        data.put("Score",score);
+        data.put("Location",location);
+        data.put("HashCode",hashcode);
+
+        CollectionReference innerCollectionRef = db.collection("Users/"+userName+"/QRCodes");
+        innerCollectionRef
+                .document(name)
+                .set(data)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d("Working", "Data added successfully");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("Working", "data not added" + e.toString());
+                    }
+                });
+
+    }
+
+    /**
+     * Returns the total score off all teh QR codes
+     */
+    public void totalScore(){
+        db.collection("Users/"+userName+"/QRCodes")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            for (QueryDocumentSnapshot document : task.getResult()){
+                                Log.d("Working", document.getId() + " => " + document.getLong("Score"));
+                                //totalSum += document.getLong("Score").intValue();
+                            }
+                        } else {
+                            Log.d("Working", "Query Unsuccessful");
+                        }
+                    }
+                });
+    }
+
+
+}
--- app/src/main/java/com/example/pokemonqrcode/MainActivity.java
+++ app/src/main/java/com/example/pokemonqrcode/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.pokemonqrcode;
 
+import static com.example.pokemonqrcode.FireStoreClass.totalSum;
+
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Pair;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.journeyapps.barcodescanner.ScanContract;
@@ -21,6 +25,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Pair<Integer, Integer> location = new Pair<>(1,1);
+        FireStoreClass f = new FireStoreClass("Kyle");
+        f.addAQRCode("Magnemite", 200, location, 234567);
+        f.addAQRCode("Bulbasaur", 340, location, 987654);
+        f.addAQRCode("charizard", 470, location, 101010);
+        f.totalScore();
+        Toast.makeText(getApplicationContext(), Long.toString(totalSum), Toast.LENGTH_SHORT).show();
 
         cameraButton = findViewById(R.id.open_camera_button);
         cameraButton.setOnClickListener(v->
