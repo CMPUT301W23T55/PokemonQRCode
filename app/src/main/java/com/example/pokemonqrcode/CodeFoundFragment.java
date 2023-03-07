03751772bf6d909ceb2141ecbba495c4915820fa
Mooftwo2 <122929359+Mooftwo2@users.noreply.github.com>
2023-03-06 18:19:04 -0700

    dialog fragment after scanning code

--- app/src/main/java/com/example/pokemonqrcode/CodeFoundFragment.java
+++ app/src/main/java/com/example/pokemonqrcode/CodeFoundFragment.java
@@ -1,16 +1,28 @@
 package com.example.pokemonqrcode;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.DialogFragment;
 
 public class CodeFoundFragment extends DialogFragment  {
@@ -25,14 +37,16 @@ public class CodeFoundFragment extends DialogFragment  {
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         View view = LayoutInflater.from(getContext()).inflate(R.layout.code_found_dialog, null);
         TextView titleText = view.findViewById(R.id.found_title);
-        TextView imgText = view.findViewById(R.id.found_picture_text);
-        ImageButton imgButton = view.findViewById(R.id.found_image_button);
-        imgButton.setOnClickListener(new View.OnClickListener() {
+        ImageView image = view.findViewById(R.id.found_user_image);
+        Button cameraButton = view.findViewById(R.id.found_open_camera);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                openSomeActivityForResult(view);
             }
         });
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         return builder
                 .setView(view)
@@ -41,7 +55,28 @@ public class CodeFoundFragment extends DialogFragment  {
 
                 })
                 .create();
+    }
 
+    public void openSomeActivityForResult(View view) {
+        ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            // There are no request codes
+                            Intent data = result.getData();
+                            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                            ImageView image = view.findViewById(R.id.found_user_image);
+                            image.setImageBitmap(bitmap);
 
+                        }
+                    }
+                });
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/jpg");
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+        //Launch activity to get result
+        someActivityResultLauncher.launch(intent);
     }
 }
