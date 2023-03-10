29a59112d84af7f34a7534d9f99ecc937f52791f
peterschefffer <pscheff8@gmail.com>
2023-03-09 17:36:40 -0700

    Added name generation. Image generation in progress

--- app/src/main/java/com/example/pokemonqrcode/PlayerCode.java
+++ app/src/main/java/com/example/pokemonqrcode/PlayerCode.java
@@ -1,5 +1,9 @@
 package com.example.pokemonqrcode;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  * This class is what shows up on the player profile
  * Create custom ListView adapter similar to GasStation in Assignment1
@@ -13,6 +17,44 @@ public class PlayerCode {
     picture
     geolocation
     comment
-
      */
+    private byte[] codeHash;
+    private int score;
+    private String name;
+    private Date date;
+    private String picture;
+    private ArrayList<String> comments;
+
+    public PlayerCode(byte[] hash, String name, int score, String image) {
+        this.codeHash = hash;
+        this.name = name;
+        this.score = score;
+        this.picture = image;
+        this.date = new Date();
+    }
+
+    public PlayerCode(byte[] hash, String name) {
+        this.codeHash = hash;
+        this.name = name;
+    }
+
+    public PlayerCode(byte[] hash) {
+        this.codeHash = hash;
+    }
+
+    public PlayerCode() {
+
+    }
+
+    public int getScore() {return this.score;}
+    public String getName() {return this.name;}
+    public String getPicture() {return this.picture;}
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPicture(String picture) {this.picture = picture;}
 }
